8e90f8cebb0d59221318fff036d4b23e431c22cc
donnaaboise <donnaaboise@users.noreply.github.com>
2025-06-07 04:26:36 +0000

    deploy: ForestClaw/forestclaw@fb6603729777201e4a6247f1b2873d0f3f8675b2

--- docs/develop/search/all_8.js
+++ docs/develop/search/all_8.js
@@ -2,6 +2,8 @@ var searchData=
 [
   ['has_5ffinegrid_5fneighbors_0',['has_finegrid_neighbors',['../structfclaw__patch__data.html#a3eb203d848e3421ba834c1ad9c777f24',1,'fclaw_patch_data']]],
   ['hash_1',['hash',['../struct__dictionary__.html#a017aee3083b2fc453bcf2d7b5b51af86',1,'_dictionary_']]],
-  ['hdf5_5fcompression_5flevel_2',['hdf5_compression_level',['../structfclaw__clawpatch__options.html#a25b0b4cb10c173bd620cfe20a4345c9e',1,'fclaw_clawpatch_options']]],
-  ['hdf5_5fpatch_5fthreshold_3',['hdf5_patch_threshold',['../structfclaw__clawpatch__options.html#afe240c69413b2e1cc885a162d8eb36f2',1,'fclaw_clawpatch_options']]]
+  ['hdf5_5faux_5fout_2',['hdf5_aux_out',['../structfclaw__clawpatch__options.html#ae58402f1e5f7c91398cfac72def43661',1,'fclaw_clawpatch_options']]],
+  ['hdf5_5faux_5fout_5fstring_3',['hdf5_aux_out_string',['../structfclaw__clawpatch__options.html#a9327a2badd62441e8280cb883c9aed39',1,'fclaw_clawpatch_options']]],
+  ['hdf5_5fcompression_5flevel_4',['hdf5_compression_level',['../structfclaw__clawpatch__options.html#a25b0b4cb10c173bd620cfe20a4345c9e',1,'fclaw_clawpatch_options']]],
+  ['hdf5_5fpatch_5fthreshold_5',['hdf5_patch_threshold',['../structfclaw__clawpatch__options.html#afe240c69413b2e1cc885a162d8eb36f2',1,'fclaw_clawpatch_options']]]
 ];
--- docs/develop/search/variables_14.js
+++ docs/develop/search/variables_14.js
@@ -5,6 +5,8 @@ var searchData=
   ['vertices_2',['vertices',['../structfclaw__block.html#a4328dcb729c6e205c7d566bb68e541b1',1,'fclaw_block::vertices'],['../structfclaw2d__block.html#a2f8420c439e45aee41ab26a708c22b98',1,'fclaw2d_block::vertices'],['../structfclaw3d__block.html#a0b3b623a8370c05159f7a9acadb48ad2',1,'fclaw3d_block::vertices']]],
   ['volume_3',['volume',['../structfclaw3d__metric__patch__t.html#a7e960df111e7a0ba9b87c45e88e02b32',1,'fclaw3d_metric_patch_t']]],
   ['vtables_4',['vtables',['../structfclaw__global.html#a124e5e13703451a0d032b9dd20bf91a1',1,'fclaw_global']]],
-  ['vtk_5fpatch_5fthreshold_5',['vtk_patch_threshold',['../structfclaw__clawpatch__options.html#a7b7702fac71db39ba4f62c85a7a6682d',1,'fclaw_clawpatch_options']]],
-  ['vtkspace_6',['vtkspace',['../structfclaw__options.html#a103e3aa363336c1598b0c976a3877467',1,'fclaw_options']]]
+  ['vtk_5faux_5fout_5',['vtk_aux_out',['../structfclaw__clawpatch__options.html#a413bf90335da4a42ad67da63f61eac21',1,'fclaw_clawpatch_options']]],
+  ['vtk_5faux_5fout_5fstring_6',['vtk_aux_out_string',['../structfclaw__clawpatch__options.html#a91bcb671eb9637cdcf0d427cd3a48b95',1,'fclaw_clawpatch_options']]],
+  ['vtk_5fpatch_5fthreshold_7',['vtk_patch_threshold',['../structfclaw__clawpatch__options.html#a7b7702fac71db39ba4f62c85a7a6682d',1,'fclaw_clawpatch_options']]],
+  ['vtkspace_8',['vtkspace',['../structfclaw__options.html#a103e3aa363336c1598b0c976a3877467',1,'fclaw_options']]]
 ];
